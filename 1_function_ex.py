c1a70331f8ffa786e40bc71a96aa43ea2c61ee2e
davidhong <taehwan.hong@gmail.com>
2017-02-20 16:28:30 +0900

    뇌를 자극하는 파이썬 7장

---
+++ 1_function_ex.py
@@ -0,0 +1,22 @@
+# 뇌를 자극하는 파이썬 ch 7 참고
+
+#절대값 변환 함수 설정
+def abs(arg):
+
+    if(arg<0):
+        result = arg * -1
+    else:
+        result = arg
+    return result
+
+#text print 함수 설정
+def print_string(text, count=1)
+    for i in range(count)
+        print(text)
+
+
+# arg 변수에 int형태로 인풋 받음
+arg = int(input("input"))
+print(abs(arg))
+
+
